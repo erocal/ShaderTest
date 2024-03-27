e019ec10890e38aec25c7a5da927125c1f9deb3e
erocal <s159752zxcvbn1@gmail.com>
2024-03-28 01:48:33 +0800

    [!][-][+] 240328

---
+++ Assets/Scripts/EarthRotate.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class EarthRotate : MonoBehaviour
+{
+    [Header("旋轉速度")]
+    [SerializeField] private float rotationSpeed = 10f;
+
+    // 傾斜角度
+    public float tiltAngle = 23.5f; // 地球傾斜角度為23.5度
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 每幀自轉物件
+        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+
+        // 傾斜自轉軸向
+        Vector3 tiltAxis = Quaternion.Euler(tiltAngle, 0f, 0f) * Vector3.up;
+        transform.Rotate(tiltAxis, rotationSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Shader/Pratice/BlendTest.shader
@@ -0,0 +1,16 @@
+Shader "Holistic/BlendTest"
+{
+    Properties
+    {
+        _MainTex("Texture", 2D) = "black" {}
+    }
+    SubShader
+    {
+        Tags { "Queue"="Transparent" }
+        Blend One One 
+        Pass {
+            SetTexture [_MainTex] { combine texture }
+        }
+    }
+    FallBack "Diffuse"
+}
